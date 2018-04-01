5582f14a14fedaa9a69ffacffd4b1a3eeead6bbf
Alon <alon_m@optimove.com>
2018-04-01 16:44:28 +0300

    Added more tests

--- test/test.js
+++ test/test.js
@@ -66,6 +66,146 @@ describe('General Translator Tests', function() {
                 sectionsPerValue.orderLT100Text;
         addToTestCases(inputObj, OrdersCorrectResult, `should return string according to order value: ${orderVal}`);
     });
+    //orders - not all if blocks have respective ifelse \ else blocks
+    var inputPageOrdersNoElse1 =
+        `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}<=200${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.orderLTOE200Text} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+
+    possibleOrderVals.forEach(function (orderVal) {
+        var OrderValAttrs = { [ORDER_KEY]: orderVal };
+        var inputObj = { [tstConsts.INPUT_KEY]: inputPageOrdersNoElse1, [consts.ATTRIBUTES_KEY_STR]: OrderValAttrs };
+        var OrdersCorrectResult = orderVal <= 200 ? sectionsPerValue.orderLTOE200Text :"";
+        addToTestCases(inputObj, OrdersCorrectResult, `should return string (no else test#1) according to order value: ${orderVal}`);
+    });
+
+    var inputPageOrdersNoElse2 =
+        `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}<=200${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.orderLTOE200Text} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}
+         ${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX}${ORDER_KEY}>100${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.orderGT100LT200Text} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+    possibleOrderVals.forEach(function (orderVal) {
+        var OrderValAttrs = { [ORDER_KEY]: orderVal };
+        var inputObj = { [tstConsts.INPUT_KEY]: inputPageOrdersNoElse2, [consts.ATTRIBUTES_KEY_STR]: OrderValAttrs };
+        var OrdersCorrectResult = orderVal <= 200 ? sectionsPerValue.orderLTOE200Text :
+            orderVal > 100 ? sectionsPerValue.orderGT100LT200Text :"";
+        addToTestCases(inputObj, OrdersCorrectResult, `should return string (no else test#2) according to order value: ${orderVal}`);
+    });
+
+    var inputPageOrdersNoElse3 =
+        `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}>=0${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.orderNNText} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}
+         ${consts.CUSTOM_ELSE_BLOCK_TAG} ${sectionsPerValue.orderNText} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+    possibleOrderVals.forEach(function (orderVal) {
+        var OrderValAttrs = { [ORDER_KEY]: orderVal };
+        var inputObj = { [tstConsts.INPUT_KEY]: inputPageOrdersNoElse3, [consts.ATTRIBUTES_KEY_STR]: OrderValAttrs };
+        var OrdersCorrectResult = orderVal >= 0 ? sectionsPerValue.orderNNText :
+            sectionsPerValue.orderNText;
+        addToTestCases(inputObj, OrdersCorrectResult, `should return string (non\negative) according to order value: ${orderVal}`);
+    });
+
+    var inputPageOrdersNoElse4 =
+        `${inputPageOrdersNoElse1} ${inputPageOrdersNoElse3}`;
+    possibleOrderVals.forEach(function (orderVal) {
+        var OrderValAttrs = { [ORDER_KEY]: orderVal };
+        var inputObj = { [tstConsts.INPUT_KEY]: inputPageOrdersNoElse4, [consts.ATTRIBUTES_KEY_STR]: OrderValAttrs };
+        var OrdersCorrectResult1 = orderVal <= 200 ? sectionsPerValue.orderLTOE200Text : "";
+        var OrdersCorrectResult2 = orderVal >= 0 ? sectionsPerValue.orderNNText :
+            sectionsPerValue.orderNText;
+        addToTestCases(inputObj, `${OrdersCorrectResult1}  ${OrdersCorrectResult2}`, `should return string  (compound no else)  according to order value: ${orderVal}`);
+    });
+
+    //languages
+    var INPUT_PAGE_LANGUAGE = `
+        ${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${LANGUAGE_KEY}=='English'${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.languageEnglishText} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}
+        ${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX}${LANGUAGE_KEY}!='Italian'${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.languageSpanishText} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}
+        ${consts.CUSTOM_ELSE_BLOCK_TAG} ${sectionsPerValue.languageItalianText} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+    var possibleLanguageVals = ["Spanish", "English", "Italian"];
+
+    possibleLanguageVals.forEach(function (langVal) {
+        var langsAttrs = { [LANGUAGE_KEY]: langVal };
+        var inputObj = { [tstConsts.INPUT_KEY]: INPUT_PAGE_LANGUAGE, [consts.ATTRIBUTES_KEY_STR]: langsAttrs };
+        var LangsCorrectResult = langVal == "English" ? sectionsPerValue.languageEnglishText :
+            langVal != "Italian" ? sectionsPerValue.languageSpanishText :
+                sectionsPerValue.languageItalianText;
+        addToTestCases(inputObj, LangsCorrectResult, `should return string according to language value: ${langVal}`);
+    });
+
+
+    /*
+    //favorite product
+    var INPUT_PAGE_STATE = `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX} ${FAV_PROD_KEY} ${consts.CUSTOM_CLOSE_DELIMITER}
+                              ${sectionsPerValue.favProductExistenceText}
+                              ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}
+                              ${consts.CUSTOM_ELSE_BLOCK_TAG} ${sectionsPerValue.favProductNonExistenceText} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+
+
+    var inputObj = { [consts.INPUT_KEY_STR]: INPUT_PAGE_STATE };
+    addToTestCases(inputObj, sectionsPerValue.favProductNonExistenceText,
+        `should return string - no favorite product provided`);
+
+    var favProdAttrs = { [FAV_PROD_KEY]: "Gucci handbag" };
+    inputObj[consts.ATTRIBUTES_KEY_STR] = favProdAttrs;
+    addToTestCases(inputObj, sectionsPerValue.favProductExistenceText,
+        `should return string with the favorite product key (${FAV_PROD_KEY})`);
+
+    //Testing case insensitivity
+
+    var INPUT_PAGE_CASE1 = `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX} ${FAV_PROD_KEY.toLowerCase()} ${consts.CUSTOM_CLOSE_DELIMITER}
+                              ${sectionsPerValue.favProductExistenceText}
+                              ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+    var INPUT_PAGE_CASE2 = `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX} ${FAV_PROD_KEY.toUpperCase()} ${consts.CUSTOM_CLOSE_DELIMITER}
+                              ${sectionsPerValue.favProductExistenceText}
+                              ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+    var INPUT_PAGE_CASE3 = `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX} ${FAV_PROD_KEY.charAt(0).toLowerCase()}${FAV_PROD_KEY.slice(1)} ${consts.CUSTOM_CLOSE_DELIMITER}
+                              ${sectionsPerValue.favProductExistenceText}
+                              ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+    var INPUT_PAGE_CASE4 = `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX} ${FAV_PROD_KEY.charAt(0).toUpperCase()}${FAV_PROD_KEY.slice(1)} ${consts.CUSTOM_CLOSE_DELIMITER}
+                              ${sectionsPerValue.favProductExistenceText}
+                              ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+
+    var inputPagesForCase = [INPUT_PAGE_CASE1, INPUT_PAGE_CASE2, INPUT_PAGE_CASE3, INPUT_PAGE_CASE4];
+
+    var favProdAttrsForCase1 = { [FAV_PROD_KEY.toLowerCase()]: "Gucci handbag" };
+    var favProdAttrsForCase2 = { [FAV_PROD_KEY.toUpperCase()]: "Gucci handbag" };
+    var favProdAttrsForCase3 = { [`${FAV_PROD_KEY.charAt(0).toLowerCase()}${FAV_PROD_KEY.slice(1)}`]: "Gucci handbag" };
+    var favProdAttrsForCase4 = { [`${FAV_PROD_KEY.charAt(0).toUpperCase()}${FAV_PROD_KEY.slice(1)}`]: "Gucci handbag" };
+
+    var favProdsForCase = [favProdAttrsForCase1, favProdAttrsForCase2, favProdAttrsForCase3, favProdAttrsForCase4];
+    inputPagesForCase.forEach(function (inputPage) {
+        favProdsForCase.forEach(function (favProd) {
+            var inputObjForCase = {
+                [consts.INPUT_KEY_STR]: inputPage,
+                [consts.ATTRIBUTES_KEY_STR]: favProd
+            };
+
+            addToTestCases(inputObjForCase, `${sectionsPerValue.favProductExistenceText}`,
+                `Checking case (sent: ${inputPage} ${favProd}).\nShould return: ${sectionsPerValue.favProductExistenceText}`);
+        });
+    });
+
+    //Testing special chars in var name
+    //'_' \ '{' '}'
+    var varName = `${FAV_PROD_KEY}_{2}`;
+    var existsTxt = "Exists!";
+    var nonExistentTxt = "Doesn't Exist!";
+    var INPUT_PAGE_SPECIAL_CHARS1 = `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${varName}${consts.CUSTOM_CLOSE_DELIMITER}
+                              ${existsTxt}
+                              ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}
+                              ${consts.CUSTOM_ELSE_BLOCK_TAG} ${nonExistentTxt} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+    var specialCharsInputObj = {
+        [consts.INPUT_KEY_STR]: INPUT_PAGE_SPECIAL_CHARS1,
+        [consts.ATTRIBUTES_KEY_STR]: { "placeHolder": "Lorem ipsum" }
+    };
+
+    //addToTestCases(specialCharsInputObj, nonExistentTxt,
+    //    `Checking special chars (sent: ${INPUT_PAGE_SPECIAL_CHARS1}).\nShould return: ${nonExistentTxt}`);
+
+    var specialCharsInputObj2 = {
+        [consts.INPUT_KEY_STR]: INPUT_PAGE_SPECIAL_CHARS1,
+        [consts.ATTRIBUTES_KEY_STR]: { [varName]: "Lorem ipsum" }
+    };
+
+    //addToTestCases(specialCharsInputObj, nonExistentTxt,
+    //    `Checking special chars (sent: ${INPUT_PAGE_SPECIAL_CHARS1} ${varName}).\nShould return: ${nonExistentTxt}`);
+
+    //Testing '[%' '%]' in var name
+    */
 
     generalTests(testCases);
 });
