157b880715c36b9b8fd1491915819a05dcaaa26b
Alon <alon_m@optimove.com>
2018-04-01 16:30:46 +0300

    Added file: testConsts.js

---
+++ test/testConsts.js
@@ -0,0 +1,10 @@
+module.exports = {
+    TST_MSG_KEY: "TstMsg",
+    CORRECT_RESULT_KEY: "correctResult",
+    INPUT_KEY: "input",
+    SHOULD_VALIDATE_KEY: "shouldValidate"
+};
+/*
+module.exports.CUSTOM_CLOSING_IF_BLOCK_TAG = `${module.exports.CUSTOM_OPEN_DELIMITER}END${module.exports.LOGICAL_CONDITION_DELIMITER}IF${module.exports.CUSTOM_CLOSE_DELIMITER}`;
+module.exports.EJS_IF_BLOCK_PREFIX = `${module.exports.DEFAULT_EJS_OPEN_DELIMITER}if (`;
+*/
