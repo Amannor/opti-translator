e0b3b7ee72472b3aa09b97e2a0c3204d3008b7fa
Alon <alon_m@optimove.com>
2018-05-30 16:40:56 +0300

    Formatted validation tests file to adhereto ESlint

--- test/validation.test.js
+++ test/validation.test.js
@@ -1,167 +1,186 @@
-'use strict';
+/* eslint-disable no-param-reassign */
 
-var expect = require('chai').expect;
+
+const expect = require('chai').expect;
 const decode = require('unescape');
-var _ = require('lodash');
-var translator = require('../index');
+const _ = require('lodash');
+const translator = require('../index');
 const consts = require('../lib/consts.js');
 const tstConsts = require('./testConsts.js');
 const tagPrefixes = require('../lib/tagPrefixes.js');
-var ValidationResult = require("../lib/validationResult.js");
+const ValidationResult = require('../lib/validationResult.js');
 
-describe('General Validation Tests', function () {
-
-    function areValidationResultsEqual(lhs, rhs) {
-        if (rhs == null) {
-            return lhs == null;
-        }
-        else if (lhs == null) {
-            return false;
-        }
-        else {
-            return lhs.isValidationEqual(rhs);
-        }
+describe('General Validation Tests', () => {
+  function areValidationResultsEqual(lhs, rhs) {
+    if (rhs == null) {
+      return lhs == null;
+    } else if (lhs == null) {
+      return false;
     }
 
-    const ORDER_KEY = "ORDER_VALUE";
-    const LANGUAGE_KEY = "LANGUAGE";
-    const FAV_PROD_KEY = "FAVORITE_PRODUCT";
-    const ALL_ATTRS_KEYS = [ORDER_KEY, LANGUAGE_KEY, FAV_PROD_KEY];
+    return lhs.isValidationEqual(rhs);
+  }
 
-    function generateEmptyLegalAttrs(defaultVal = "") {
-        let attrsObj = {};
-        ALL_ATTRS_KEYS.forEach(function (attrKey) {
-            attrsObj[attrKey] = defaultVal;
-        });
-        return attrsObj;
+  function areValidationResultsEqualIgnoreDetails(lhs, rhs) {
+    if (lhs && lhs.details) {
+      lhs.details = '';
     }
 
-    var tstObjs = [];
-
-    function executeGeneralValidationTests() {
-        tstObjs.forEach(function (testObj) {
-            var tstMsg = testObj[tstConsts.TST_MSG_KEY];
-            var testInputStr = testObj[tstConsts.INPUT_KEY];
-            var expectedValidationResults = testObj[consts.VALIDATE_OUTPUT_KEY_STR];
-            var maxClausesPerBlock = testObj[consts.VALIDATION_INPUT_KEY_MAX_CONDS_PER_BLOCK];
-            var maxBlocksPerTemplate = testObj[consts.VALIDATION_INPUT_KEY_MAX_BLOCKS_PER_TEMPLATE];
-            var attrs = testObj[consts.ATTRIBUTES_KEY_STR];
-
-
-            it(tstMsg, function () {
-                var result = translator.translate(testInputStr, attrs, true, maxClausesPerBlock, maxBlocksPerTemplate);
-                var actualValidationResults = result[consts.VALIDATE_OUTPUT_KEY_STR];
-                expect(_.differenceWith(expectedValidationResults, actualValidationResults, areValidationResultsEqual).length).to.equal(0);
-                expect(_.differenceWith(actualValidationResults, expectedValidationResults, areValidationResultsEqual).length).to.equal(0);
-            });
-        });
+    if (rhs && rhs.details) {
+      rhs.details = '';
     }
+    return areValidationResultsEqual(lhs, rhs);
+  }
+  const ORDER_KEY = 'ORDER_VALUE';
+  const LANGUAGE_KEY = 'LANGUAGE';
+  const FAV_PROD_KEY = 'FAVORITE_PRODUCT';
+  const ALL_ATTRS_KEYS = [ORDER_KEY, LANGUAGE_KEY, FAV_PROD_KEY];
 
-    function addToValidationTests(msgToAdd, inputToAdd, resultsToAdd, attrsObj = generateEmptyLegalAttrs(), maxClausesPerBlock = null, maxBlocksPerTemplate = null) {
-        var tstObj = {};
-        tstObj[tstConsts.TST_MSG_KEY] = msgToAdd;
-        tstObj[tstConsts.INPUT_KEY] = inputToAdd;
-        tstObj[consts.VALIDATE_OUTPUT_KEY_STR] = resultsToAdd;
-        tstObj[consts.VALIDATION_INPUT_KEY_MAX_CONDS_PER_BLOCK] = maxClausesPerBlock;
-        tstObj[consts.VALIDATION_INPUT_KEY_MAX_BLOCKS_PER_TEMPLATE] = maxBlocksPerTemplate;
-        tstObj[consts.ATTRIBUTES_KEY_STR] = attrsObj;
+  function generateEmptyLegalAttrs(defaultVal = '') {
+    const attrsObj = {};
+    ALL_ATTRS_KEYS.forEach((attrKey) => {
+      attrsObj[attrKey] = defaultVal;
+    });
+    return attrsObj;
+  }
 
-        tstObjs.push(tstObj);
-    }
+  const tstObjs = [];
 
-    addToValidationTests('should return empty validation list', "", []);
+  function executeGeneralValidationTests() {
+    tstObjs.forEach((testObj) => {
+      const tstMsg = testObj[tstConsts.TST_MSG_KEY];
+      const testInputStr = testObj[tstConsts.INPUT_KEY];
+      const expectedValidationResults = testObj[consts.VALIDATE_OUTPUT_KEY_STR];
+      const maxClausesPerBlock = testObj[consts.VALIDATION_INPUT_KEY_MAX_CONDS_PER_BLOCK];
+      const maxBlocksPerTemplate = testObj[consts.VALIDATION_INPUT_KEY_MAX_BLOCKS_PER_TEMPLATE];
+      const attrs = testObj[consts.ATTRIBUTES_KEY_STR];
 
-    const sectionsPerValue = {
-        orderGT200Text: "Order greater than 200",
-        orderLTOE200Text: "Order less than or equal to 200",
-        orderGT100LT200Text: "Order greater than 100 and less than 200",
-        orderLT100Text: "Order less than 100",
-        orderNNText: "Order non-negative",
-        orderNText: "Order negative",
-        languageEnglishText: "Content for English",
-        languageNotEnglishText: "Content not for English",
-        languageSpanishText: "Content for Spanish",
-        languageNotSpanishText: "Content not for Spanish",
-        languageItalianText: "Content for Italian",
-        favProductExistenceText: `Your favorite product: ${consts.CUSTOM_OPEN_DELIMITER}${FAV_PROD_KEY}${consts.CUSTOM_CLOSE_DELIMITER}`,
-        favProductNonExistenceText: `No favorite product`
-    };
+
+      it(tstMsg, () => {
+        const result = translator.translate(testInputStr, attrs, true, maxClausesPerBlock, maxBlocksPerTemplate);
+        const actualValidationResults = result[consts.VALIDATE_OUTPUT_KEY_STR];
+        expect(_.differenceWith(expectedValidationResults, actualValidationResults, areValidationResultsEqualIgnoreDetails).length).to.equal(0);
+        expect(_.differenceWith(actualValidationResults, expectedValidationResults, areValidationResultsEqualIgnoreDetails).length).to.equal(0);
+      });
+    });
+  }
+
+  function addToValidationTests(msgToAdd, inputToAdd, resultsToAdd, attrsObj = generateEmptyLegalAttrs(), maxClausesPerBlock = null, maxBlocksPerTemplate = null) {
+    const tstObj = {};
+    tstObj[tstConsts.TST_MSG_KEY] = msgToAdd;
+    tstObj[tstConsts.INPUT_KEY] = inputToAdd;
+    tstObj[consts.VALIDATE_OUTPUT_KEY_STR] = resultsToAdd;
+    tstObj[consts.VALIDATION_INPUT_KEY_MAX_CONDS_PER_BLOCK] = maxClausesPerBlock;
+    tstObj[consts.VALIDATION_INPUT_KEY_MAX_BLOCKS_PER_TEMPLATE] = maxBlocksPerTemplate;
+    tstObj[consts.ATTRIBUTES_KEY_STR] = attrsObj;
+
+    tstObjs.push(tstObj);
+  }
+
+  addToValidationTests('should return empty validation list', '', []);
+
+  const sectionsPerValue = {
+    orderGT200Text: 'Order greater than 200',
+    orderLTOE200Text: 'Order less than or equal to 200',
+    orderGT100LT200Text: 'Order greater than 100 and less than 200',
+    orderLT100Text: 'Order less than 100',
+    orderNNText: 'Order non-negative',
+    orderNText: 'Order negative',
+    languageEnglishText: 'Content for English',
+    languageNotEnglishText: 'Content not for English',
+    languageSpanishText: 'Content for Spanish',
+    languageNotSpanishText: 'Content not for Spanish',
+    languageItalianText: 'Content for Italian',
+    favProductExistenceText: `Your favorite product: ${consts.CUSTOM_OPEN_DELIMITER}${FAV_PROD_KEY}${consts.CUSTOM_CLOSE_DELIMITER}`,
+    favProductNonExistenceText: 'No favorite product',
+  };
 
     // A single [%END:IF%} missing
-    var inputStr = `
+  var inputStr = `
         ${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}>200${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.orderGT200Text}`;
-    var res = new ValidationResult(consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
-        `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}>200${consts.CUSTOM_CLOSE_DELIMITER}`, `${consts.VALIDATION_BLOCK_MSG} - missing required closing clause ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`);
-    addToValidationTests(`Should return a ${res.errorMsg} - missing ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`, inputStr, [res]);
+  var res = new ValidationResult(
+    consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
+    `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}>200${consts.CUSTOM_CLOSE_DELIMITER}`, `${consts.VALIDATION_BLOCK_MSG} - missing required closing clause ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`,
+  );
+  addToValidationTests(`Should return a ${res.errorMsg} - missing ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`, inputStr, [res]);
 
 
-    // More blocks than allowed
-    var inputStr = `
+  // More blocks than allowed
+  var inputStr = `
         ${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}>200${consts.CUSTOM_CLOSE_DELIMITER} ${sectionsPerValue.orderGT200Text} ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
 
-    inputStr = `${inputStr} ${inputStr} ${inputStr}`;
-    var maxAllowedCondBlocksNum = 2;
-    var validationMsg = `${consts.VALIDATION_BLOCK_MSG}: block #${maxAllowedCondBlocksNum + 1} exceeds max number of allowed blocks: ${maxAllowedCondBlocksNum}`;
-    var res = new ValidationResult(maxAllowedCondBlocksNum, consts.VALIDATION_START_INDEX,
-        `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}>200${consts.CUSTOM_CLOSE_DELIMITER}`, validationMsg);
-    addToValidationTests(`Should return: ${res.errorMsg}`, inputStr, [res], generateEmptyLegalAttrs(), 999, maxAllowedCondBlocksNum);
+  inputStr = `${inputStr} ${inputStr} ${inputStr}`;
+  const maxAllowedCondBlocksNum = 2;
+  const validationMsg = `${consts.VALIDATION_BLOCK_MSG}: block #${maxAllowedCondBlocksNum + 1} exceeds max number of allowed blocks: ${maxAllowedCondBlocksNum}`;
+  var res = new ValidationResult(
+    maxAllowedCondBlocksNum, consts.VALIDATION_START_INDEX,
+    `${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}>200${consts.CUSTOM_CLOSE_DELIMITER}`, validationMsg,
+  );
+  addToValidationTests(`Should return: ${res.errorMsg}`, inputStr, [res], generateEmptyLegalAttrs(), 999, maxAllowedCondBlocksNum);
 
 
-    // Compound valid tag prefixes
-    tagPrefixes.PersonalizationTypesPrefixes.forEach(function (prefix) {
-
-        var curKeys = [`${prefix}${consts.LOGICAL_CONDITION_DELIMITER}A`,
-            `${prefix}${consts.LOGICAL_CONDITION_DELIMITER}A${consts.LOGICAL_CONDITION_DELIMITER}B`,
-            `${prefix}${consts.LOGICAL_CONDITION_DELIMITER}A${consts.LOGICAL_CONDITION_DELIMITER}B${consts.LOGICAL_CONDITION_DELIMITER}C`
-        ];
-        curKeys.forEach(function (curKey) {
-            var inputStr = `                                                                                                                                                        
+  // Compound valid tag prefixes
+  tagPrefixes.PersonalizationTypesPrefixes.forEach((prefix) => {
+    const curKeys = [`${prefix}${consts.LOGICAL_CONDITION_DELIMITER}A`,
+      `${prefix}${consts.LOGICAL_CONDITION_DELIMITER}A${consts.LOGICAL_CONDITION_DELIMITER}B`,
+      `${prefix}${consts.LOGICAL_CONDITION_DELIMITER}A${consts.LOGICAL_CONDITION_DELIMITER}B${consts.LOGICAL_CONDITION_DELIMITER}C`,
+    ];
+    curKeys.forEach((curKey) => {
+      const inputStr = `                                                                                                                                                        
                    ${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${curKey}>200${consts.CUSTOM_CLOSE_DELIMITER} body ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
-            addToValidationTests(`Valid prefixes checked: ${curKey}`, inputStr, [], {[curKey]: ""});
-        });
+      addToValidationTests(`Valid prefixes checked: ${curKey}`, inputStr, [], { [curKey]: '' });
     });
+  });
 
-    //Operators testing (both legal & illegal)
-    consts.LEGAL_OPERATORS.forEach(function (op) {
-        let inputStr = `
+  // Operators testing (both legal & illegal)
+  consts.LEGAL_OPERATORS.forEach((op) => {
+    const inputStr = `
                ${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}${op}200${consts.CUSTOM_CLOSE_DELIMITER} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
-        addToValidationTests(`Legal operator: ${op}`, inputStr, []);
-    });
-    let illegalOps = ["=", "=!", "=!!", "=!!!", "!", "!!"];
-    illegalOps.forEach(function (op) {
-        let inputStr = `
+    addToValidationTests(`Legal operator: ${op}`, inputStr, []);
+  });
+  const illegalOps = ['=', '=!', '=!!', '=!!!', '!', '!!'];
+  illegalOps.forEach((op) => {
+    const inputStr = `
                ${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${ORDER_KEY}${op}200${consts.CUSTOM_CLOSE_DELIMITER} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
-        let invalidCond = inputStr.substring(inputStr.indexOf(consts.CUSTOM_OPENING_IF_BLOCK_PREFIX) + consts.CUSTOM_OPENING_IF_BLOCK_PREFIX.length,
-            inputStr.indexOf(consts.CUSTOM_CLOSE_DELIMITER) + consts.CUSTOM_CLOSE_DELIMITER.length);
-        invalidCond = invalidCond.replace(consts.CUSTOM_CLOSE_DELIMITER, "").replace(consts.CUSTOM_OPEN_DELIMITER, "").trim();
-        var res = new ValidationResult(consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
-            invalidCond, consts.VALIDATION_COND_MSG);
-        addToValidationTests(`Illegal operator: ${op}`, inputStr, [res]);
-    });
+    let invalidCond = inputStr.substring(
+      inputStr.indexOf(consts.CUSTOM_OPENING_IF_BLOCK_PREFIX) + consts.CUSTOM_OPENING_IF_BLOCK_PREFIX.length,
+      inputStr.indexOf(consts.CUSTOM_CLOSE_DELIMITER) + consts.CUSTOM_CLOSE_DELIMITER.length,
+    );
+    invalidCond = invalidCond.replace(consts.CUSTOM_CLOSE_DELIMITER, '').replace(consts.CUSTOM_OPEN_DELIMITER, '').trim();
+    const res = new ValidationResult(
+      consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
+      invalidCond, consts.VALIDATION_COND_MSG,
+    );
+    addToValidationTests(`Illegal operator: ${op}`, inputStr, [res]);
+  });
 
-    //Invalid attribute
-    let invalidAttr = "ddddddddddddd";
-    consts.LEGAL_OPERATORS.forEach(function (op) {
-        let invalidCond = `${invalidAttr}${consts.OP_EQ}'Italian'`;
-        var inputStr = `
+  // Invalid attribute
+  const invalidAttr = 'ddddddddddddd';
+  consts.LEGAL_OPERATORS.forEach((op) => {
+    const invalidCond = `${invalidAttr}${consts.OP_EQ}'Italian'`;
+    const inputStr = `
         ${consts.CUSTOM_OPENING_IF_BLOCK_PREFIX}${invalidCond}${consts.CUSTOM_CLOSE_DELIMITER} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}
         ${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX}${LANGUAGE_KEY}${consts.OP_EQ}'English'${consts.CUSTOM_CLOSE_DELIMITER} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
-        var res = new ValidationResult(consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
-            invalidCond, consts.VALIDATION_COND_MSG);
-        addToValidationTests(`Invalid attribute (legal operator: ${op})`, inputStr, [res]);
-    });
+    const res = new ValidationResult(
+      consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
+      invalidCond, consts.VALIDATION_COND_MSG,
+    );
+    addToValidationTests(`Invalid attribute (legal operator: ${op})`, inputStr, [res]);
+  });
 
-    //Invalid block order - simple
-    var elseifBlock = `${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX}${LANGUAGE_KEY}${consts.OP_EQ}'English'${consts.CUSTOM_CLOSE_DELIMITER}`;
-    var elseifFirstInputStr = `${elseifBlock} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
-    var res = new ValidationResult(consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX, elseifBlock, consts.VALIDATION_ILLEGAL_BLOCK_ORDER_MSG);
-    addToValidationTests(`Invalid block order: ${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX} before IF`, elseifFirstInputStr, [res]);
+  // Invalid block order - simple
+  const elseifBlock = `${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX}${LANGUAGE_KEY}${consts.OP_EQ}'English'${consts.CUSTOM_CLOSE_DELIMITER}`;
+  const elseifFirstInputStr = `${elseifBlock} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+  var res = new ValidationResult(consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX, elseifBlock, consts.VALIDATION_ILLEGAL_BLOCK_ORDER_MSG);
+  addToValidationTests(`Invalid block order: ${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX} before IF`, elseifFirstInputStr, [res]);
 
-    var elseFirstInputStr = `${consts.CUSTOM_ELSE_BLOCK_TAG} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
-    res = new ValidationResult(consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
-        consts.CUSTOM_ELSE_BLOCK_TAG, consts.VALIDATION_ILLEGAL_BLOCK_ORDER_MSG);
-    addToValidationTests(`Invalid block order: ${consts.CUSTOM_ELSE_BLOCK_TAG} before IF`, elseFirstInputStr, [res]);
-/*
+  const elseFirstInputStr = `${consts.CUSTOM_ELSE_BLOCK_TAG} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
+  res = new ValidationResult(
+    consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
+    consts.CUSTOM_ELSE_BLOCK_TAG, consts.VALIDATION_ILLEGAL_BLOCK_ORDER_MSG,
+  );
+  addToValidationTests(`Invalid block order: ${consts.CUSTOM_ELSE_BLOCK_TAG} before IF`, elseFirstInputStr, [res]);
+  /*
     var inputStr = `
         ${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX}${LANGUAGE_KEY}${consts.OP_EQ}'English'${consts.CUSTOM_CLOSE_DELIMITER} Lorem ipsum ${consts.CUSTOM_CLOSING_IF_BLOCK_TAG}`;
     var res = new ValidationResult(consts.VALIDATION_START_INDEX, consts.VALIDATION_START_INDEX,
@@ -170,8 +189,8 @@ describe('General Validation Tests', function () {
     addToValidationTests(`Invalid block order: ${consts.CUSTOM_OPENING_ELSEIF_BLOCK_PREFIX} before IF`, `${inputStr} ${in}`, [res]);
 
 */
-    //Invalid block order - compound (meaning smthing like if.... else... elseif / if...else...else)
-    //TODO...
+  // Invalid block order - compound (meaning smthing like if.... else... elseif / if...else...else)
+  // TODO...
 
-    executeGeneralValidationTests();
+  executeGeneralValidationTests();
 });
